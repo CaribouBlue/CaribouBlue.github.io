1eac2329659e448c6bd4933e0572f8c024ba0054
Alec Draymore <Draymore26@gmail.com>
2017-09-13 16:33:48 -0400

    first draft

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import Contact from './components/Contact';
+import {
+  BrowserRouter as Router,
+  Route, Switch,
+  Redirect
+} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route
+        exact path="/"
+        component={App}
+      />
+      <Route
+        exact path="/contact"
+        component={Contact}
+      />
+      <Route
+        render={() => <Redirect to="/" />}
+      />
+    </Switch>
+  </Router>, document.getElementById('root'));
